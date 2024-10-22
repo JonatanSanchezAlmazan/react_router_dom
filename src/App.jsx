0bcf3dfe45a9301e89afabae51353777106d64d5
Jonatan Sanchez <jonatansan1@hotmail.com>
2024-10-22 08:27:44 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/about"}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/contact"}>Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
